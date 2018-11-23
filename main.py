58953efa38f16ce63e0c698df513d9313c68cf63
diana <diana3372@gmail.com>
2018-11-23 13:55:05 +0100

    Questions working

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import pandas as pd
-
+import random
 
 class Disorder:
     def __init__(self, name, id):
@@ -64,3 +64,32 @@ for symptom_id, row in enumerate(symptoms_questions_df.iterrows()):
     symptoms_to_questions[symptom_id] = question
     
 
+# System run
+print('Answer to each question with a Yes(Y) or No(N) according to whether the symptom is present in the patient.\n')
+
+# First shuffle questions
+symptom_question_tuples = list(symptoms_to_questions.items())
+random.shuffle(symptom_question_tuples)
+
+n_questions = 5
+count_questions = 0
+
+present_symptoms = []
+for symptom_id, question in symptom_question_tuples:
+    print(question)
+    while True:   
+        answer = input('Yes/No >> ')
+        answer = answer.lower()
+        if answer not in ['yes', 'no', 'y', 'n']:
+            print('Incorrect input. Please answer Yes or No')
+        else:
+            break
+    
+    if 'y' in answer:
+        present_symptoms.append(symptom_id)
+        
+    count_questions += 1
+    if count_questions == n_questions:
+        break
+    
+print([symptoms_ids[x] for x in ypresent_symptoms])
